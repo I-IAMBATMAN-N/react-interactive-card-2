c9f0b57be3e255984537620745b8f81822b9cd38
JB <93525600+I-IAMBATMAN-N@users.noreply.github.com>
2023-11-01 12:03:53 +0100

    [FIX] card fill function

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,4 +1,20 @@
-export default function Cards({ number, name, month, year, cvc }) {
+export default function Cards({ name, number, month, year, cvc }) {
+  function fillNumber() {
+    let string = number;
+    if (number.length > 0) {
+      for (let i = 0; i < 19 - number.length; i++) {
+        if (string.length === 4 || string.length === 9 || string.length === 14)
+          string += " ";
+        else string += "0";
+      }
+      // if (number.length === 4 || number.length === 9 || number.length === 14) {
+      //   string += " ";
+      // }
+      // return setNumber(String(e.target.value) + string);
+    }
+
+    return string;
+  }
   return (
     <>
       <section className="card front-card">
@@ -6,18 +22,27 @@ export default function Cards({ number, name, month, year, cvc }) {
           <span></span>
           <span id="logo-smaller"></span>
         </div>
-        <p className="card-front-number">{number}</p>
+        <p className="card-holder-number">
+          {number.length > 0 ? fillNumber() : "0000 0000 0000 0000"}
+        </p>
         <div className="card-bottom-info-container">
-          <p className="card-holder-name">{name}</p>
+          <p className="card-front-name">
+            {name.length > 0 ? name : "Jane Appleseed"}
+          </p>
           <p className="expiration-date">
-            <span className="expiration-date-month">{month}</span>/
-            <span className="expiration-date-year">{year}</span>
+            <span className="expiration-date-month">
+              {month.length > 0 ? month : "00"}
+            </span>
+            /
+            <span className="expiration-date-year">
+              {year.length > 0 ? year : "00"}
+            </span>
           </p>
         </div>
       </section>
       <section className="card back-card">
         <div className="tape"></div>
-        <p className="card-ccv">{cvc}</p>
+        <p className="card-ccv">{cvc.length > 0 ? cvc : "000"}</p>
         <div className="bar-design"></div>
       </section>
     </>
